4a6daf2128d612c8633eb3629d793570d126e286
oliversugic <osugic@gmail.com>
2022-07-30 19:19:13 +0200

    Sugic - added Topic Resource

---
+++ backend/src/main/java/boundary/TopicResource.java
@@ -0,0 +1,52 @@
+package boundary;
+
+import entity.Topic;
+import org.jboss.resteasy.reactive.RestPath;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import java.util.List;
+
+@Path("/api/topic")
+public class TopicResource {
+
+    @GET
+    @Path("getAll")
+    public List<Topic> list() {
+        return Topic.listAll();
+    }
+
+    @GET
+    @Path("{id}")
+    public Topic getById(long id) {
+        return Topic.findById(id);
+    }
+
+    @POST
+    @Transactional
+    @Path("addTopic")
+    public Topic add(Topic topic) {
+        topic.persistAndFlush();
+        return topic;
+    }
+
+    @DELETE
+    @Transactional
+    @Path("deleteTopic/{id}")
+    public void delete(@RestPath long id) {
+        Topic.findById(id).delete();
+    }
+
+    @PATCH
+    @Transactional
+    @Path("editTopic/{id}")
+    public Topic edit(@RestPath long id, Topic topic) {
+        Topic tmp = Topic.findById(id);
+        tmp.setComment(topic.getComment());
+        tmp.setEvent(topic.getEvent());
+        tmp.setName(topic.getName());
+        tmp.setPreviousTopic(topic.getPreviousTopic());
+        tmp.persistAndFlush();
+        return topic;
+    }
+}
