98807dfe2d833decbc2ba6fcca5d189455aca38c
oliversugic <osugic@gmail.com>
2022-07-30 19:20:20 +0200

    Sugic - added Repository Test class

---
+++ backend/src/test/java/control/RepositoryTests.java
@@ -0,0 +1,14 @@
+package control;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+
+@QuarkusTest
+class RepositoryTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+}
