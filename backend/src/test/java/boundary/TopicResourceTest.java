672afa5e4b797fe9ff9749a72810a1c6887c2dbf
oliversugic <osugic@gmail.com>
2022-08-08 18:19:46 +0200

    Sugic - added TopicResourceTests / all green

---
+++ backend/src/test/java/boundary/TopicResourceTest.java
@@ -0,0 +1,106 @@
+package boundary;
+
+import entity.Event;
+import entity.Topic;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.transaction.Transactional;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class TopicResourceTest {
+
+    long topicId;
+
+    @BeforeEach
+    @Transactional
+    public void setup() {
+        Topic.deleteAll();
+        Topic topic = new Topic();
+        topic.setName("topic");
+        topic.setComment("comment");
+        topic.setEvent(new Event());
+        topic.setEvent(new Event());
+        topic.persistAndFlush();
+        topicId = topic.getId();
+    }
+
+    @Test
+    void listAllTopics() {
+        given()
+                .when().get("/api/topic/getAll")
+                .then()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @Test
+    void getTopicById() {
+        given()
+                .when().get("/api/topic/" + topicId)
+                .then()
+                .statusCode(200)
+                .body("name ", is("topic"));
+
+    }
+
+    @Test
+    void addTopic() {
+        given()
+                .body(Map.of("name", "topic", "comment", "comment"))
+                .contentType(ContentType.JSON)
+                .when().post("/api/topic/addTopic")
+                .then()
+                .statusCode(200)
+                .body("name", is("topic"))
+                .body("comment", is("comment"));
+
+        given()
+                .when().get("/api/topic/getAll")
+                .then()
+                .statusCode(200)
+                .body("[1].name", is("topic"))
+                .body("[1].comment", is("comment")); // second of array because one is always persisted
+    }
+
+    @Test
+    void deleteTopic() {
+        given()
+                .when().delete("/api/topic/deleteTopic/" + topicId)
+                .then()
+                .statusCode(204);
+
+        given()
+                .when().get("/api/topic/getAll")
+                .then()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+
+    @Test
+    void editTopic() {
+        given()
+                .body(Map.of("name", "Editname", "comment", "Editcomment"))
+                .contentType(ContentType.JSON)
+                .when().patch("/api/topic/editTopic/" + topicId)
+                .then()
+                .statusCode(200)
+                .body("name", is("Editname"))
+                .body("comment", is("Editcomment"));
+
+        given()
+                .when().get("/api/topic/" + topicId)
+                .then()
+                .statusCode(200)
+                .body("name", is("Editname"))
+                .body("comment", is("Editcomment"));
+    }
+}
