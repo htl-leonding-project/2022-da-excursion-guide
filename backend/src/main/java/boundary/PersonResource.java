ed49b326ab2f415130e0054cf15fdae3a623c07c
oliversugic <osugic@gmail.com>
2022-09-23 15:48:42 +0200

    Sugic - added endpoint

--- backend/src/main/java/boundary/PersonResource.java
+++ backend/src/main/java/boundary/PersonResource.java
@@ -3,6 +3,8 @@ package boundary;
 import entity.Person;
 import org.jboss.resteasy.reactive.RestPath;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -11,17 +13,20 @@ import javax.ws.rs.core.Response;
 @Path("api/person")
 public class PersonResource {
 
+    @Inject
+    EntityManager em;
+
     @GET
     @Path("getAll")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response list(){
+    public Response list() {
         return Response.ok(Person.listAll()).build();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getById(long id){
+    public Response getById(long id) {
         return Response.ok(Person.findById(id)).build();
     }
 
@@ -30,7 +35,7 @@ public class PersonResource {
     @Path("addPerson")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response add(Person person){
+    public Response add(Person person) {
         person.persistAndFlush();
         return Response.ok(person).build();
     }
@@ -38,14 +43,14 @@ public class PersonResource {
     @DELETE
     @Transactional
     @Path("deletePerson/{id}")
-    public void delete(@RestPath long id){
+    public void delete(@RestPath long id) {
         Person.findById(id).delete();
     }
 
     @PATCH
     @Transactional
     @Path("editPerson/{id}")
-    public Response edit(@RestPath long id, Person person){
+    public Response edit(@RestPath long id, Person person) {
         Person tmp = Person.findById(id);
         tmp.setComment(person.getComment());
         tmp.setFirstname(person.getFirstname());
@@ -54,6 +59,17 @@ public class PersonResource {
         tmp.setTelephone(person.getTelephone());
         tmp.persistAndFlush();
         return Response.ok(person).build();
+    }
+
+    @GET
+    @Path("/getByName/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getByName(@RestPath String name) {
+        return Response.
+                ok(em.createQuery("SELECT p from Person p WHERE p.lastname Like :NAME")
+                        .setParameter("NAME", name)
+                        .getResultList()
+                ).build();
     }
 
 
