84849b9361d9f53fd69d834af99f091373d1973e
oliver <osugic@gmail.com>
2022-09-27 17:07:05 +0200

    Sugic - added method to get Topic

--- backend/src/main/java/control/TopicRepository.java
+++ backend/src/main/java/control/TopicRepository.java
@@ -4,8 +4,19 @@ import entity.Topic;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 @ApplicationScoped
 public class TopicRepository implements PanacheRepository<Topic> {
 
+    @Inject
+    EntityManager em;
+
+    public Topic getTopic(String topicname){
+        return em
+                .createQuery("SELECT t from Topic t where t.name LIKE :NAME", Topic.class)
+                .setParameter("NAME", topicname)
+                .getSingleResult();
+    }
 }
