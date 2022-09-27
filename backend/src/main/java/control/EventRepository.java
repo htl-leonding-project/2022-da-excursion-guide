81f10c4fa4ce247283e900829a16d7d14fbe37b4
oliver <osugic@gmail.com>
2022-09-27 09:16:52 +0200

    Sugic - added find Method

--- backend/src/main/java/control/EventRepository.java
+++ backend/src/main/java/control/EventRepository.java
@@ -6,8 +6,18 @@ import entity.Person;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 @ApplicationScoped
 public class EventRepository implements PanacheRepository<Event> {
+    @Inject
+    EntityManager em;
+    public Event getEvent(String eventname){
+        return em
+                .createQuery("SELECT e from Event e where e.name LIKE :NAME",Event.class)
+                .setParameter("NAME",eventname)
+                .getSingleResult();
+    }
 
 }
