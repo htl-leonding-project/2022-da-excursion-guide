4f8c9adf7e9375e01ead855b654d67ec49cbb58f
oliver <osugic@gmail.com>
2022-10-20 12:14:26 +0200

    Sugic - added Interfaces

---
+++ frontend/excursion-guide/src/app/intefaces/event.ts
@@ -0,0 +1,13 @@
+import {Person} from "./person";
+import {Topic} from "./topic";
+
+export interface Event {
+  id:                  number;
+  maxPersonAllowed:    number;
+  location:                string;
+  participant:         Person[];
+  planedEndDateTime:   Date;
+  planedStartDateTime: Date;
+  topics:              Topic[];
+  type:                string;
+}
