a973d419d6ae9c217626fc0d1521c516eae51c06
oliver <osugic@gmail.com>
2022-12-01 11:24:01 +0100

    Sugic - updated unlock component

--- frontend/excursion-guide/src/app/components/unlock-activity/unlock-activity.component.ts
+++ frontend/excursion-guide/src/app/components/unlock-activity/unlock-activity.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Event} from "../../model/event";
+import {EventserviceService} from "../../services/eventservice.service";
+import {Activity} from "../../model/activity";
+import {ActivityDto} from "../../model/activityDto";
+import {Topic} from "../../model/topic";
 
 @Component({
   selector: 'app-unlock-activity',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnlockActivityComponent implements OnInit {
 
-  constructor() { }
+  list: Event[] = [];
 
-  ngOnInit(): void {
+  constructor(private eventService: EventserviceService) {
   }
 
+  ngOnInit(): void {
+    this.getAllEvents();
+  }
+
+  getAllEvents() {
+    this.eventService.getAllEvents()
+      .subscribe((data: Event[]) => {
+        this.list = data;
+      });
+  }
+
+  changeVisibility(activity: Activity, topic: Topic) {
+    let activityDto: ActivityDto = {
+      id: activity.id,
+      activityName: activity.activityName,
+      comment: activity.comment,
+      latitude: activity.latitude,
+      longitude: activity.longitude,
+      startDateTime: activity.startDateTime,
+      publicationDate: activity.publicationDate,
+      isPublic: !activity.public,
+      previousActivityId: activity.previousActivity?.id || 0,
+      topicId: topic.id
+    };
+
+
+    activity.public = !activity.public;
+    this.eventService.updateActivity(activityDto)
+      .subscribe((data: ActivityDto) => {
+        this.getAllEvents();
+      });
+  }
 }
