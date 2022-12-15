152740e87bb8b1b2d2729c1b55293039e90f4e06
oliver <osugic@gmail.com>
2022-12-15 08:31:35 +0100

    Sugic - small update in .json and .ts files

--- frontend/excursion-guide/src/app/app.module.ts
+++ frontend/excursion-guide/src/app/app.module.ts
@@ -7,11 +7,16 @@ import {TourViewComponent} from './components/tour-view/tour-view.component';
 import {TourCreationComponent} from "./components/tour-creation/tour-creation.component";
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {UnlockActivityComponent} from './components/unlock-activity/unlock-activity.component';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatTableModule} from "@angular/material/table";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 const appRoutes: Routes = [
   {path: 'view', component: TourViewComponent},
@@ -36,7 +41,15 @@ const appRoutes: Routes = [
     FormsModule,
     MatSlideToggleModule,
     MatTableModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports:[
   ],
   providers: [],
   bootstrap: [AppComponent]
