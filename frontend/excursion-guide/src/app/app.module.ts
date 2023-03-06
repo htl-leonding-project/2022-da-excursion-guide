d2f6896f3f88f19feb04e8fd717f5b3d7e8e402f
oliver <osugic@gmail.com>
2023-03-06 23:43:37 +0100

    Sugic - added to app module

--- frontend/excursion-guide/src/app/app.module.ts
+++ frontend/excursion-guide/src/app/app.module.ts
@@ -8,19 +8,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {UnlockActivityComponent} from './components/unlock-activity/unlock-activity.component';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatTableModule} from "@angular/material/table";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {MatStep, MatStepLabel, MatStepperModule} from "@angular/material/stepper";
-import {MatButtonModule} from "@angular/material/button";
-import {MatSelectModule} from "@angular/material/select";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
 import { TourCreateComponent } from './components/tour-create/tour-create.component';
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatNativeDateModule} from "@angular/material/core";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 const appRoutes: Routes = [
@@ -44,20 +32,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     HttpClientModule,
     FormsModule,
-    MatSlideToggleModule,
-    MatTableModule,
-    MatButtonToggleModule,
-    MatNativeDateModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatCheckboxModule,
-    MatExpansionModule,
-    MatStepperModule
   ],
   exports:[
   ],
