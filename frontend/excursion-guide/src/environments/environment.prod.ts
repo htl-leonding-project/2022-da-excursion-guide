4ea964692c7d69b19c7c2803c7d41bcd7bb4fd65
oliver <osugic@gmail.com>
2023-03-07 13:02:35 +0100

    Sugic - update prod

--- frontend/excursion-guide/src/environments/environment.prod.ts
+++ frontend/excursion-guide/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://student.cloud.htl-leonding.ac.at/o.sugic/leoTour-frontend/api',
+  url: 'https://student.cloud.htl-leonding.ac.at/o.sugic/leotour-backend/api',
 };
